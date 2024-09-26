8876c77714dc19474bd614bc6a63c20c4974d8ea
Ariel Guzman <GuzmanAriel525@gmail.com>
2024-09-26 20:46:46 +0000

    Completed build of multi-select

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,176 @@
-// add to the top
-import React from "react";
 import { createRoot } from "react-dom/client";
-
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed),
-  ]);
-};
+import React, { useEffect, useState } from "react";
+import data from "/data.json";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Cockatiel",
-    }),
-    React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mix" }),
-  ]);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [selectedOptions, setselectedOptions] = useState([]);
+  const DOWN_ARROW_KEY_CODE = 40;
+  const UP_ARROW_KEY_CODE = 38;
+  const ENTER_KEY_CODE = 13;
+  const SPACE_KEY_CODE = 32;
+  const ESCAPE_KEY_CODE = 32;
+
+  const toggleDropdownVisibility = (e) => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
+  const toggleCheckedCheckboxes = (e, value) => {
+    const isKeyboardEvent =
+      e.type === "keyup" &&
+      (e.which === ENTER_KEY_CODE || e.which === SPACE_KEY_CODE);
+    const isClickEvent = e.type === "click";
+
+    if (!isKeyboardEvent && !isClickEvent) return;
+
+    setselectedOptions((previousState) => {
+      let updatedState = [...previousState];
+
+      if (value === "280560009") {
+        // Clear all selections and toggle 'not applicable' option
+        return updatedState.includes(value) ? [] : [value];
+      }
+
+      // Remove 'not applicable' if it's present
+      updatedState = updatedState.filter((option) => option !== "280560009");
+
+      // Toggle the current value
+      if (updatedState.includes(value)) {
+        updatedState = updatedState.filter((option) => option !== value);
+      } else {
+        updatedState = [...updatedState, value];
+      }
+
+      return updatedState;
+    });
+  };
+
+  return (
+    <div className="gv__container">
+      <div className="gv__form__item">
+        <div className="gv__form__label" id="gradesTaughtLabel">
+          Grades Taught *
+        </div>
+        <div className="gv__form__checkbox-dropdown">
+          <div
+            className="gv__select-input__input-wrapper gv__form__checkbox-dropdown-select"
+            onClick={toggleDropdownVisibility}
+            aria-haspopup="true"
+            aria-controls="checkbox-list"
+            aria-expanded="false"
+            role="combobox"
+            tabIndex="0"
+          >
+            <div className="gv__select-input__field gv__form__field">
+              {selectedOptions.length == 0 ? (
+                <span
+                  className="js-default-label"
+                  aria-labelledby="checkbox-dropdown"
+                >
+                  {" "}
+                  Select Grades
+                </span>
+              ) : null}
+              <div className="js-token-container">
+                {data.FormData.Options.map((item, i) => {
+                  if (selectedOptions.includes(item.Value)) {
+                    return (
+                      <button
+                        className="checkbox-dropdown__token"
+                        tabIndex="0"
+                        data-value={item.Value}
+                        key={item.Value}
+                        onClick={(e) => {
+                          toggleCheckedCheckboxes(e, item.Value);
+                        }}
+                        onKeyUp={(e) => {
+                          toggleCheckedCheckboxes(e, item.Value);
+                        }}
+                      >
+                        {item.Text}
+                      </button>
+                    );
+                  }
+                }).filter((item) => !!item)}
+              </div>
+            </div>
+          </div>
+          {isDropdownOpen && (
+            <div
+              className="gv__form__checkbox-dropdown-list"
+              role="checkbox-list"
+              id="checkbox-list"
+              aria-labelledby="checkbox-dropdown"
+              aria-multiselectable="true"
+            >
+              {data.FormData.Options.map((item, i) => {
+                return (
+                  <div
+                    className="gv__form__item gv__checkbox-layout gv__form__item--checkbox"
+                    key={`grades-taught-${i}`}
+                    id={`grades-taught-${i}`}
+                    onClick={(e) => {
+                      toggleCheckedCheckboxes(e, item.Value);
+                    }}
+                    onKeyUp={(e) => {
+                      toggleCheckedCheckboxes(e, item.Value);
+                    }}
+                    role="checkbox"
+                    tabIndex={0}
+                    aria-checked={
+                      selectedOptions.includes(item.Value) ? true : false
+                    }
+                  >
+                    <input
+                      type="checkbox"
+                      data-type="checkbox"
+                      id={`gradesTaught-${item.Text}`}
+                      className="js-class-fieldset-grades-taught-input gv__form__field"
+                      value={item.Value}
+                      data-label-text={item.Text}
+                      tabIndex="-1"
+                      defaultChecked={
+                        selectedOptions.includes(item.Value) ? true : false
+                      }
+                      data-checked={
+                        selectedOptions.includes(item.Value) ? true : false
+                      }
+                    />
+                    <label
+                      className="gv__form__label"
+                      htmlFor="gradesTaught"
+                      id={`grades-taught-label-${i}`}
+                      tabIndex={0}
+                    >
+                      {item.Text}
+                    </label>
+                  </div>
+                );
+              })}
+            </div>
+          )}
+        </div>
+        <input
+          type="hidden"
+          data-type="checkbox-array"
+          name="gradesTaught"
+          className="js-stored-checkbox-values"
+          aria-required="true"
+          defaultValue={JSON.stringify(selectedOptions)}
+          value={JSON.stringify(selectedOptions)}
+        />
+        <span
+          aria-atomic="true"
+          className="gv__form__error-msg"
+          id="gradesTaughtError"
+        >
+          Please select a grade level
+        </span>
+      </div>
+    </div>
+  );
 };
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(React.createElement(App));
+root.render(<App />);
